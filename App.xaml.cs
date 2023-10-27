49624401e2879943f4a7b822c552cdb55d13eb4e
Ywint92 <ynwint@student.rtc.edu>
2023-10-27 12:09:22 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab___Text_Boxes__Labels__and_Buttons
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
