5fb5f5200eb4e6a2e35a8716ac891ec9ace17bae
Ywint92 <ynwint@student.rtc.edu>
2023-10-27 13:47:03 -0700

    lab

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Lab___Text_Boxes__Labels__and_Buttons
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
